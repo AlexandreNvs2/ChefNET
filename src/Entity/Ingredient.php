c35ba83ece4ad08f1801cd35ef267c0f061f238f
Alexandre <alexandre.neves0402@gmail.com>
2024-01-09 23:43:57 +0100

    Création de lobjet Ingredient, mise en place des Fixtures

---
+++ src/Entity/Ingredient.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\IngredientRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: IngredientRepository::class)]
+class Ingredient
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    //Assert pour Gérer le Minimum et le Maximum possible.
+    #[Assert\Length(min: 2, max: 50)]
+    #[Assert\NotBlank]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    //Assert pour que le Prix soit forcément positif
+    #[Assert\Range(
+        minMessage: "Le prix ne peut pas être infèrieur a 0",
+        maxMessage: "Le prix ne peut pas être supèrieur à 200",
+        min: 0.01,
+        max: 200)]
+    #[Assert\NotNull]
+    #[Assert\NotBlank]
+    private ?float $price = null;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    #[Assert\NotNull]
+    private ?\DateTimeImmutable $createdAt ;
+
+    //Constructeur de DateTimeImmutable lors d'un CreatedAt
+    public  function __construct()
+    {
+
+        $this->createdAt = new \DateTimeImmutable();
+    }
+    public function getId(): ?int
+
+    {
+        
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
