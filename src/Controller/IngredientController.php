420adf4bf8ce3f618e97fbec82effb3e5de34d17
AlexandreNvs2 <alexandre.neves0402@gmail.com>
2024-03-06 20:49:50 +0100

    require paginator + pagination dans IngredientController.php

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -3,20 +3,28 @@
 namespace App\Controller;
 
 use App\Repository\IngredientRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IngredientController extends AbstractController
 {
     #[Route('/ingredient', name: 'app_ingredient')]
-    public function index(IngredientRepository $repository/* Injection de dépendance*/): Response
-    {
-        $ingredients = $repository->findAll();
+    public function index(IngredientRepository $repository,PaginatorInterface $paginator, Request $request
+        /* Injection de dépendance(Ici Paginator et Repository)*/): Response
 
+    {
+
+        /* Ici on paramètre notre pagination avec les query et du nombre de query par pages (ici 10 par page)*/
+        $ingredients = $paginator->paginate(
+            $repository->findAll(),  /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            10 ); /*limit per page*/
 
         return $this->render('pages/ingredient/index.html.twig', [
-            'ingredients' => $ingredients, //Ici on fait passer notre ingrédients en vue
+            'ingredients' => $ingredients //Ici on fait passer notre ingrédients en vue
         ]);
     }
 }
